4ff23ecf8f2ad0c6d5d2cabc68ce2425679123ef
krmanis <kr_manish@outlook.com>
2015-01-10 21:53:11 +0530

    after first round

--- server.js
+++ server.js
@@ -1,12 +1,67 @@
-var express = require('express');
+var express = require('express'),
+  stylus = require('stylus'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser')
+  var mongoose = require('mongoose');
+
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://localhost/MEANVision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error.......'));
+//open the database for once
+db.once('open', function callback(){
+	console.log('MEANVision db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message',  messageSchema);
+
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+	if(err){
+		console.log(err);
+	}
+	mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
 
 app.get('*', function (req, res) {
-	res.render('index');
+	res.render('index', {
+		mongoMessage: mongoMessage
+	});
 })
+
+var port = 3003
+
+app.listen(port);
+console.log("MEANVision Server started running on: "+ port);
