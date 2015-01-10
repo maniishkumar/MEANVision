9391040913e37510ae1e71fd365138892ebfb71f
krmanis <kr_manish@outlook.com>
2015-01-10 22:59:16 +0530

    'heroku'

---
+++ public/app/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('app', ['ngResource', 'ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+	  enabled: true,
+	  requireBase: false
+	});
+    $routeProvider
+        .when('/', { templateUrl: '/partials/main', controller: 'mainCtrl'});
+});
+
+app.controller('mainCtrl', function($scope) {
+    $scope.myVar = "Hello Angular, Lets explore the world";
+});
